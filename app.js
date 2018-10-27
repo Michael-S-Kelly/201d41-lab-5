c7d2c089424cddf9dc662e08617c234fb68385b1
Michael-S-Kelly <mike.s.kelly@icloud.com>
2018-10-26 17:17:05 -0700

    Completed question 5

--- app.js
+++ app.js
@@ -61,22 +61,34 @@ testSumAndMultiply(4,7,5);
 /* Problem 4
 Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
 
-"2,3,4 was passed in as an array of numbers, and 9 is their sum."
+2,3,4 was passed in as an array of numbers, and 9 is their sum.
+2,3,4 was passed in as an array of numbers, and 9 is their sum.
+2,3,4 was passed in as an array of numbers, and 9 is their sum.
 
 IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To do addition, use your sum() function that you've already created. You're going to have to be resourceful to figure out how to do this.
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
-
+//console.log('before function');
 // Write your code here
 var testArray = [2, 3, 4]; //eslint-disable-line
 
+
 function sumArray(sumArr) { //eslint-disable-line
+  var sumofArray = 0;
+  //console.log('after function');
+  for(var counter = 0; counter < sumArr.length; counter++) {
+    sumofArray = sumofArray + sumArr[counter];
+  }
+  //console.log(sumofArray);
+  //console.log(sumArr[0], sumArr[1], sumArr[2]);
+  console.log(sumofArray , sumArr[0] + ',' + sumArr[1] + ',' + sumArr[2] + ' was passed in as an array of numbers, and ' + sumofArray + ' is their sum.');
+  return [sumofArray , sumArr[0] + ',' + sumArr[1] + ',' + sumArr[2] + ' was passed in as an array of numbers, and ' + sumofArray + ' is their sum.'];
 
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -92,13 +104,12 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
 // testMultiplyArray(testArray);
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
 // You're done! Submit the link to the repo following the instructions in Canvas. Or, try out the stretch goal below...
 
